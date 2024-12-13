31fd5defa7a9d8d02685ab611ec0c6ef7ae7a5f9
KavinKumar008 <kavikumarsraja@gmail.com>
2024-12-13 22:19:21 +0530

    infinite scroll for image done

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,7 +2,7 @@ import { IoSearch } from "react-icons/io5";
 import { suggessitions } from "../utils/Suggessition";
 // import { images } from "../images/Image";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { proposal } from "../proposal/Proposal";
 import { pixabayResponse } from "../types/pixabyResponse";
 
@@ -12,8 +12,10 @@ const HomePage = () => {
 
   const [storedApiData, setStoredApiData] = useState<pixabayResponse[]>([]);
   const [title, setTitle] = useState("photo");
-  // const [clickedImages, setClickedImages] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const observerTarget = useRef(null);
   // console.log(suggessitions);
   // console.log(import.meta.env.VITE_API_KEY);
 
@@ -21,64 +23,71 @@ const HomePage = () => {
   // let url = `https://pixabay.com/api/?key=${key}&q=&image_type=photo&per_page=20&safesearch=true`;
   // console.log(url);
 
-  async function handleCallApi() {
+  async function handleCallApi(page: number) {
+    console.log("handlecallapi");
+    if (isLoading) return;
+    setIsLoading(true);
     try {
       const response = await axios.get(
-        `https://pixabay.com/api/?key=${key}&image_type=photo&per_page=20&safesearch=true`
+        `https://pixabay.com/api/?key=${key}&image_type=photo&per_page=20&safesearch=true&page=${page}`
       );
       if (response.status === 200) {
-        setStoredApiData(response.data.hits);
+        // setStoredApiData(response.data.hits);
+        setStoredApiData((prev) => [...prev, ...response.data.hits]);
+        // setPage((prevPage) => prevPage + 1);
       }
     } catch (error) {
       console.log(error);
-    }
-  }
-
-  // async function handleSuggesitionsApi() {
-  //   try {
-  //     const response = await axios.get(
-  //       `https://pixabay.com/api/?key=${key}&q=${clickedImages}&image_type=photo&per_page=20&safesearch=true`
-  //     );
-  //     if (response.status === 200) {
-  //       setStoredApiData(response.data.hits);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  async function handleInputApi() {
-    try {
-      const response = await axios.get(
-        `https://pixabay.com/api/?key=${key}&q=${inputValue}&image_type=photo&per_page=20&safesearch=true`
-      );
-      if (response.status === 200) {
-        setStoredApiData(response.data.hits);
-      }
-    } catch (error) {
-      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
-    handleCallApi();
+    handleCallApi(page);
   }, []);
 
-  // useEffect(() => {
-  //   handleSuggesitionsApi();
-  // }, [clickedImages]);
+  console.log(page);
 
-  async function handleClickData(item: string) {
-    // setClickedImages(item);
+  async function handleInputApi() {
+    console.log("handleInputApi");
+    if (isLoading) return;
+    setIsLoading(true);
     try {
       const response = await axios.get(
-        `https://pixabay.com/api/?key=${key}&q=${item}&image_type=photo&per_page=20&safesearch=true`
+        `https://pixabay.com/api/?key=${key}&q=${inputValue}&image_type=photo&per_page=20&safesearch=true&page=${page}`
       );
       if (response.status === 200) {
-        setStoredApiData(response.data.hits);
+        // setStoredApiData(response.data.hits);
+        setStoredApiData((prev) => [...prev, ...response.data.hits]);
+        // setPage((prevPage) => prevPage + 1);
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleClickData(item: string) {
+    // setClickedImages(item);
+    console.log("handleClickData");
+
+    if (isLoading) return;
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=${key}&q=${item}&image_type=photo&per_page=20&safesearch=true&page=${page}`
+      );
+      if (response.status === 200) {
+        // setStoredApiData(response.data.hits);
+        setStoredApiData((prev) => [...prev, ...response.data.hits]);
+        setPage((prevPage) => prevPage + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -104,36 +113,147 @@ const HomePage = () => {
   async function handleProposal(type: string) {
     // setImageType(item);
     setTitle(type);
+    if (isLoading) return;
+    setIsLoading(true);
     try {
       const response = await axios.get(
         type === "video"
-          ? `https://pixabay.com/api/videos/?key=${videoKey}&video_type=${type}&per_page=20&safesearch=true`
-          : `https://pixabay.com/api/?key=${key}&image_type=${type}&per_page=20&safesearch=true`
+          ? `https://pixabay.com/api/videos/?key=${videoKey}&video_type=${type}&per_page=20&safesearch=true&page=${page}`
+          : `https://pixabay.com/api/?key=${key}&image_type=${type}&per_page=20&safesearch=true&page=${page}`
       );
       if (response.status === 200) {
         // console.log(response.data.hits[0].videos.medium.url);
         // setVideosApiData(response.data.hits);
+        // type === "video"
+        //   ? setStoredApiData(response.data.hits)
+        //   : setStoredApiData(response.data.hits);
+        // console.log(response.data.hits);
         type === "video"
-          ? setStoredApiData(response.data.hits)
-          : setStoredApiData(response.data.hits);
-        console.log(response.data.hits);
+          ? setStoredApiData((prev) => [...prev, ...response.data.hits])
+          : setStoredApiData((prev) => [...prev, ...response.data.hits]);
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
-
-  // functs
-
-  console.log(title);
-
-  // console.log(proposal.map((item) => item));
 
   const imagess = proposal
     .filter((item) => item.qurey === title)
     .map((item) => (item.qurey === title ? item.image : ""));
 
-  console.log(imagess);
+  // console.log(imagess);
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       if (entries[0].isIntersecting) {
+  //         console.log("Observer target is in view, loading more data");
+  //         console.log("hiiiii");
+  //         setPage((prevPage) => prevPage + 1);
+  //         // handleCallApi();
+  //         // handleInputApi();
+  //         handleProposal(type);
+  //       }
+  //     },
+  //     {
+  //       root: null, // Default to the viewport
+  //       rootMargin: "0px", // Trigger when the target element is fully in view
+  //       threshold: 1.0, // Trigger when the target element is fully visible
+  //     }
+  //   );
+
+  //   if (observerTarget.current) {
+  //     observer.observe(observerTarget.current);
+  //   }
+
+  //   return () => {
+  //     if (observerTarget.current) {
+  //       observer.unobserve(observerTarget.current);
+  //     }
+  //   };
+  // }, [observerTarget]);
+
+  // useEffect(() => {
+  //   console.log("heloooooooooooooooooo");
+  //   if (observerTarget.current) {
+  //     handleScroll();
+  //   }
+  // }, [isLoading]);
+  // console.log(page);
+
+  // function handleScroll() {
+  //   // console.log("hiiiiiiiiiiii");
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       if (entries[0].isIntersecting) {
+  //         console.log("Observer target is in view, loading more data");
+  //         setPage((prevPage) => prevPage + 1);
+  //         // console.log(page);
+  //         handleCallApi();
+  //         // handleInputApi();
+  //       }
+  //     },
+  //     {
+  //       root: null, // Default to the viewport
+  //       rootMargin: "0px", // Trigger when the target element is fully in view
+  //       threshold: 1.0, // Trigger when the target element is fully visible
+  //     }
+  //   );
+
+  //   if (observerTarget.current) {
+  //     observer.observe(observerTarget.current);
+  //   }
+
+  //   return () => {
+  //     if (observerTarget.current) {
+  //       observer.unobserve(observerTarget.current);
+  //     }
+  //   };
+  // }
+
+  // useEffect(() => {
+  //   console.log("byeeeeeeeeeeeeeeeeeeeeeee");
+  //   handleCallApi();
+  // }, [page]);
+
+  // useEffect(() => {
+  //   handleCallApi();
+  // }, [page]);
+
+  // Function to check if we are at the bottom of the page
+
+  const checkIfAtBottom = () => {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const bottomPosition = document.documentElement.scrollHeight;
+    return scrollPosition >= bottomPosition - 1;
+  };
+
+  // Load more data when reaching the bottom
+  // useEffect(() => {
+  //   if (!isLoading && checkIfAtBottom()) {
+  //     // setPage((prevPage) => prevPage + 1); // Increment page number for next fetch
+  //     handleCallApi(page);
+  //   }
+  // }, [isLoading, page]);
+
+  // Event listener to handle scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!isLoading && checkIfAtBottom()) {
+        setPage((prevPage) => prevPage + 1); // Increment page number for next fetch
+        handleCallApi(page);
+        console.log("hiiiii");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isLoading, page]);
 
   return (
     <main
@@ -226,11 +346,8 @@ const HomePage = () => {
       </section>
       <section className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4 p-4">
         {storedApiData &&
-          storedApiData.map((item) => (
-            <div
-              key={item.id}
-              className="cursor-pointer mb-4 break-inside-avoid"
-            >
+          storedApiData.map((item, ind) => (
+            <div key={ind} className="cursor-pointer mb-4 break-inside-avoid">
               {item.type === "film" ? (
                 <video controls>
                   <source
@@ -250,6 +367,14 @@ const HomePage = () => {
             </div>
           ))}
       </section>
+      {/* <div
+        ref={observerTarget}
+        className="invisible h-1"
+        onChange={handleScroll}
+      ></div> */}
+      <div>
+        <p>No more to Load.......</p>
+      </div>
       {/* <section className="columns-1 sm:columns-2 lg:columns-3 p-4 gap-4">
         {videosApiData &&
           videosApiData.map((item) => (
